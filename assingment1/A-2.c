258724132081e5973de27f357a9a7f15353701ac
Apparao Gangade <swamimd19.comp@coep.ac.in>
2020-08-15 21:18:08 +0530

    commit on assignment

---
+++ assingment1/A-2.c
@@ -0,0 +1,21 @@
+int Z;
+
+void f()
+{
+        int a, b, c,x;
+        a = Z;
+        if (a < 10) {
+                b = 5;
+                c = 17;
+        } else {
+                b = 6;
+                c = 20;
+                if (a == 0) {
+                        c = 0;
+                }
+        }
+        Z = b * 10 + c;
+        x=a?b:c;
+
+}
+
